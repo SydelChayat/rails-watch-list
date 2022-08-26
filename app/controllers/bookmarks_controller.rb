bd740496b1df208c55f20ff44651c2839f1dd855
Sydel Chayat <mayoindonela@yahoo.com>
2022-08-26 14:29:35 +0300

    done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,15 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+end
